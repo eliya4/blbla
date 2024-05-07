e4cba3d33e8bd17dda8027a2dbc8b1a8a058aecf
eliya <eliyacaspi4@gmail.com>
2024-05-07 10:30:14 +0300

    hahaha

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        FirstFragment firstFragment= FirstFragment.newInstance();
-       getSupportFragmentManager().beginTransaction().replace(R.id.button,firstFragment).commit();
+       SECANDFragment secandFragment=SECANDFragment.newInstance();
+        Button fragOneButn= findViewById(R.id.button);
+        Button fragTowButn= findViewById(R.id.button2);
+
+
+        fragOneButn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.button,firstFragment).commit();
+            }
+        });
+        fragTowButn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.button,secandFragment).commit();
+
+            }
+        });
+
     }
+
 }
